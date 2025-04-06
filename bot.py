ea6af1070effbe81332eef91ba949f5b97a63801
YuvicHaha <97956492+YuvicHaha@users.noreply.github.com>
2025-04-06 20:17:47 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # List of allowed domains
-ALLOWED_DOMAINS = {"example.com", "trustedsite.com"}
+ALLOWED_DOMAINS = {"cdn.discordapp.com", "trustedsite.com"}
 
 # Role name that allows sending links
 ALLOWED_ROLE_NAME = "link perms"  # Change this to match the actual role name
